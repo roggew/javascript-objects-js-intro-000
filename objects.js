f9e01d42dcb86d107530dc014ca753eab6aac4bc
William Rogge <williamroggeiv@gmail.com>
2020-04-10 20:12:07 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+
+var playlist = {"My Bloody Valentine": "Sometimes"}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist [Phil Ochs] = "Here's to the State of Mississippi"
+  playlist.Slowdive = 'Alison'
+  return playlist
+}
