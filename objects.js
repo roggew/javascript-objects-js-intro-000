086a5eaa6bcb14bded00583974977e175d3d3253
William Rogge <williamroggeiv@gmail.com>
2020-04-10 20:12:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@
 var playlist = {"My Bloody Valentine": "Sometimes"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist [Phil Ochs] = "Here's to the State of Mississippi"
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   playlist.Slowdive = 'Alison'
   return playlist
 }
